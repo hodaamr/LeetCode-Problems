5f8af2aee8be77584216da5e2e16bdb92c29103f
Hoda Amr <48572265+hodaamr@users.noreply.github.com>
2022-05-25 02:36:49 +0200

    Time: 197 ms (66.51%), Space: 34.7 MB (55.12%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.kt
+++ 28-implement-strstr/28-implement-strstr.kt
@@ -2,36 +2,28 @@ class Solution {
     fun strStr(haystack: String, needle: String): Int {
         
         if(needle.isEmpty()) return 0
-        else if(haystack.contains(needle)) {
-            return haystack.substringBefore(needle).length
-        } else return -1
-     
-        /*var i: Int = 0
-        var j: Int = 0
         
-        
-        if (haystack.length == 1 || needle.length == 0) {
-            return  0
-        }
-        
-        else if (needle in haystack){
-            
-            while (i < haystack.length && j < needle.length){
-               
-                if (haystack[i] == needle[j]){
-                    break
-                    
+        for ((index, char) in haystack.withIndex()){
+            return if (char == needle[0]){
+                if(haystack.length >= index + needle.length){
+                    if(haystack.substring(index, index + needle.length) == needle){
+                        index
+                    } 
+                    else{
+                        continue
+                        
+                    }
                 }
                 else {
-                    i++
+                    -1
                 }
-                
             }
-            return i        
+            else{
+                continue
+            }
         }
-        else {
-             return -1
-         }*/
+        
+        return -1
         
     }
 }
