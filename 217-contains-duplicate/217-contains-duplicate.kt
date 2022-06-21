36dd5f05ebcc083a0f506861b30b342104d96957
Hoda Amr <48572265+hodaamr@users.noreply.github.com>
2022-06-22 00:58:23 +0200

    Time: 785 ms (35.18%), Space: 75.5 MB (16.31%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.kt
+++ 217-contains-duplicate/217-contains-duplicate.kt
@@ -1,9 +1,15 @@
 class Solution {
     fun containsDuplicate(nums: IntArray): Boolean {
         
+        // Solution idea: 
+        // 1. Sort given array -> nums
+        // 2. Compare two adjacent elements
+        // 3. if they are equal, return true
+        // 4. else return false
+        
         var i = 0
         
-        nums.sort()
+        nums.sort() 
         
         while (i < nums.size-1) {
             
