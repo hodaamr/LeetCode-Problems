1c65dce4236697871d556c90d07454f7622690fc
Hoda Amr <48572265+hodaamr@users.noreply.github.com>
2022-07-25 15:28:37 +0200

    Time: 462 ms (5.78%), Space: 48.5 MB (5.20%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.kt
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.kt
@@ -3,28 +3,15 @@ class Solution {
         
         val result: IntArray = intArrayOf(0 , 0)
         
-        
-        result[0] = nums.indexOfFirst{it == target}
-        result[1] = nums.indexOfLast{it == target}
-        
-        /*if (nums.isEmpty()){
+        if (nums.isEmpty()){
             result[0] = -1
             result[1] = -1
             
         }
         
-        for ((index, value) in nums.withIndex()){
-            if (value == target && nums[index+1] == target){
-                result[0] = index
-                result[1] = index + 1
-                break
-            }
-            else {
-                result[0] = -1
-                result[1] = -1
-            }
+        result[0] = nums.indexOfFirst{it == target}
+        result[1] = nums.indexOfLast{it == target}
         
-        }*/
         
         return result
     }
